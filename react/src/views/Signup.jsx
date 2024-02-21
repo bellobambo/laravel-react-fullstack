e2bb2fc80900276c590f69af7bae47d63dbb8ed6
bellobambo <bellobambo21@gmail.com>
2024-02-21 18:02:53 +0100

    add

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,52 +1,51 @@
-import React, { useRef } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import axiosClient from '../axios-client';
 import { useStateContext } from '../contexts/ContextProvider';
 
-
-
-
 const Signup = () => {
-
   const nameRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmationRef = useRef();
-
-  const { setUser, setToken } = useStateContext()
+  const [errors, setErrors] = useState(null);
+  const { setUser, setToken } = useStateContext();
 
   const onSubmit = (e) => {
-    // debugger;
-    e.preventDefault()
+    e.preventDefault();
 
     const payload = {
       name: nameRef.current.value,
       email: emailRef.current.value,
       password: passwordRef.current.value,
       passwordConfirmation: passwordConfirmationRef.current.value
-    }
+    };
 
-    console.log(payload)
     axiosClient.post('/signup', payload)
       .then(({ data }) => {
-        setToken(data.user)
-        setToken(data.token)
+        setToken(data.token);
+        setUser(data.user);
       })
       .catch(err => {
         const response = err.response;
         if (response && response.status === 422) {
-          // response.data.errors
-          console.log(response.data.errors);
+          setErrors(response.data.errors);
         }
-      })
-
-  }
+      });
+  };
 
   return (
     <div className='login-signup-form animated fadeInDown'>
       <div className="form">
         <form onSubmit={onSubmit} action="" >
           <h1 className='title'> Sign Up For Free </h1>
+          {errors && (
+            <div className='alert'>
+              {Object.keys(errors).map(key => (
+                <p key={key}>{errors[key]}</p>
+              ))}
+            </div>
+          )}
           <input ref={nameRef} placeholder='Full Name' />
           <input ref={emailRef} type="email" placeholder='Email' />
           <input ref={passwordRef} type="password" placeholder='Password' />
@@ -58,7 +57,7 @@ const Signup = () => {
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
